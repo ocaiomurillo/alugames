13923ef3fbef912872977b4074ea6f3bd73a19f3
Caio Murillo Oliveira <caio@BRUDDENPO.LOCAL>
2024-09-08 13:50:44 -0300

    atualizar app.js - projeto final

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function alterarStatus(id){
+  let gameClicado = document.getElementById(`game-${id}`);
+  let imagem = gameClicado.querySelector('.dashboard__item__img');
+  let botao = gameClicado.querySelector('.dashboard__item__button');
+  let nomeJogo = gameClicado.querySelector('.dashboard__item__name').textContent;
+
+
+  if(imagem.classList.contains('dashboard__item__img--rented')){
+    if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo}?`)){
+    imagem.classList.remove('dashboard__item__img--rented');
+    botao.classList.remove('dashboard__item__button--return');
+    botao.textContent = 'Alugar';
+  }
+  } else{
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.classList.add('dashboard__item__button--return');
+    botao.textContent = 'Devolver';
+  }
+}
